b679edc290f4e1d0e11c55e24ded3b33357798a8
Davit Martirosyan <dvmartirosyan@gmail.com>
2019-04-28 12:27:12 +0400

    add connection to mongoose -> init.js, add server connection to PORT 3000

---
+++ init/init.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './env/.env'});
+
+mongoose.connect(process.env.db_path, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+}).then(
+    () => { console.log('Connected to the database successfully!') },
+    err => { console.error('Failed to connect to the database with error:', err); }
+);
