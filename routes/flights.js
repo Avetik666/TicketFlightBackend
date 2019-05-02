e423cae81d533794e7cd825c3a257d13a0d19971
Avetik <avetik.sarikyan@picsart.com>
2019-05-03 00:40:40 +0400

    add variable input through url parameters using ? variable name divided by &

--- routes/flights.js
+++ routes/flights.js
@@ -9,26 +9,15 @@ const router = express.Router();
 const appId = '1a04733c';
 const appKey = '9707581d0840cc6b8ed9d61128915b0f';
 
-const flightCode = 'AA';
-const flightNumber = '100';
-const year = '2019';
-const month = '4';
-const day = '27';
-
-const requestURL = `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${flightCode}/${flightNumber}/dep/${year}/${month}/${day}?appId=${appId}&appKey=${appKey}&utc=false`;
-
 // request.get(requestURL,function(err,res,body1){
 //     body1 = JSON.parse(body1);
 //     console.log(body1.request);
 // });
 
-async function asyncRequest() {
+async function asyncRequest(requestURL) {
     return new Promise((resolve, reject) => (
         request.get(requestURL, function (err, res, body) {
-                // console.log(body);
-                // console.log(JSON.parse(body));
                 const output = JSON.parse(body);
-                console.log(output.flightStatuses[0].airportResources.departureTerminal);
                 resolve(JSON.parse(body));
             }
         )
@@ -36,8 +25,19 @@ async function asyncRequest() {
 }
 router.get('/', async function (req, res, next) {
     try {
-        const body = await asyncRequest();
-        console.log(body);
+        // console.log(req.query.flightCode);
+        // console.log(req.query.flightNumber);
+        // console.log(req.query.year);
+        // console.log(req.query.month);
+        // console.log(req.query.day);
+        const flightCode = req.query.flightCode;
+        const flightNumber = req.query.flightNumber;
+        const year = req.query.year;
+        const month = req.query.month;
+        const day = req.query.day;
+        let requestURL = `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${flightCode}/${flightNumber}/dep/${year}/${month}/${day}?appId=${appId}&appKey=${appKey}&utc=false`;
+
+        const body = await asyncRequest(requestURL);
         await Flight.createFlight(body);
         res.json();
 
